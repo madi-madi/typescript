9ca9993fd373db705c1a447cd8075ec18357e865
madi-madi <ssss_-_2010@hotmail.com>
2018-08-07 15:41:03 +0300

    class promise, array , map ...

--- typescript-base.js
+++ typescript-base.js
@@ -51,4 +51,132 @@ var obj2 = __assign({ x: 'ibr' }, obj1);
 console.log(obj2);
 var f = obj1.first, l = obj1.last;
 console.log(f + ' ' + l);
+// Array 
+var arr = ['a', 'b'];
+var x = arr[0], y = arr[1];
+// console.log(arr);
+var options = { x: '1', c: '2' };
+// Function Parameter Destructuring
+function func(options) {
+    console.log('Options X ' + options.x);
+}
+func(options);
+function fun(_a) {
+    var x = _a.x;
+    console.log(x);
+}
+fun({ x: "Forsan Test" });
+//For Loop
+for (var i = 0; i < arr.length; i++) { //var ,let
+    console.log("For loop ES5 : " + arr[i]);
+}
+//For loop ES6 
+for (var _i = 0, arr_1 = arr; _i < arr_1.length; _i++) {
+    var i = arr_1[_i];
+    console.log("For Loop ES6 : " + i);
+}
+//Foreach
+arr.forEach(function (value) {
+    console.log(" This value with forEach ES5: " + value);
+});
+// For in Designed for interating over an object properites
+for (var i_1 in obj1) {
+    console.log("For in  for used in object : " + i_1);
+}
+// Map & Set
+var base = { a: 1, b: 2 };
+var objbase = Object.create(base);
+// objbase[c] = 4;
+for (i in objbase) {
+    console.log(i);
+}
+console.log("Object,create Error !!! " + objbase);
+var objoverriding = { hasOwnProperty: "test" };
+//  Error : TypeError: objoverriding.hasOwnProperty is not a function
+// objoverriding.hasOwnProperty("test Ovrriding");
+console.log(" Error !!! : " + objoverriding.hasOwnProperty);
+var base1 = { a: 1, __proto__: 2 };
+for (i in base1) {
+    console.log("Property __proto__ !!!: " + i);
+}
+// Map 
+var map = new Map([['A', 3], ['c', 4]]);
+console.log("Empty " + map.get("A"));
+map.set('A', 2);
+// 
+console.log("Set " + map.get("A")); // overriding
+console.log("Set " + map.get("c"));
+// for of looping
+for (var _a = 0, _b = map.entries(); _a < _b.length; _a++) {
+    var entry = _b[_a];
+    console.log("For loop map  :  " + entry);
+}
+// for loop key & Value 
+for (var _c = 0, _d = map.entries(); _c < _d.length; _c++) {
+    var _e = _d[_c], key = _e[0], value = _e[1];
+    console.log("Used Foreach to print key & Value with entries: " + key + " -> " + value + " ");
+}
+for (var _f = 0, map_1 = map; _f < map_1.length; _f++) {
+    var _g = map_1[_f], key = _g[0], value = _g[1];
+    console.log("Used Foreach to print key & Value : " + key + " -> " + value + " ");
+}
+for (var _h = 0, _j = map.keys(); _h < _j.length; _h++) {
+    var key = _j[_h];
+    console.log("Map map.keys() : " + key);
+}
+for (var _k = 0, _l = map.values(); _k < _l.length; _k++) {
+    var value = _l[_k];
+    console.log("Map map.values() : " + value);
+}
+// We can check to see if a key is present using the has method:
+console.log("We can check to see if  a key is present using the has method : " +
+    map.has("A"));
+console.log("We can check to see if  a key is present using the has method : " +
+    map.has("l"));
+// We can check for the size (number of entries) using the size property:
+console.log("We can check for thr size using the size method : " +
+    map.size);
+// We can delete entries using the delete method:
+console.log("We can delete entries using the delete method: " +
+    map["delete"]("A"));
+// We can empty an entire Map by using the clear method:
+console.log("We can empty an entries map by using the clear method: " +
+    map.clear());
+// Set 
+console.log('Sets are a bit like maps but they only store keys not key-value pairs.');
+var set = new Set();
+set.add('Apple');
+console.log(set);
+// Api Promise
+var error = true;
+function doAsyncTask() {
+    return new Promise(function (resolve, reject) {
+        setTimeout(function () {
+            if (error) {
+                // code...
+                reject('error promise');
+            }
+            else {
+                resolve('done promise');
+            }
+        }, 900);
+    });
+}
+doAsyncTask().then(function (val) { return console.log(val); }, function (err) { return console.log(err); });
+// Class
+// class Person{
+// 	firstName = "";
+// 	lastName = "";
+// 	constractor(firstName , lastName){
+// 		this.firstName = firstName;
+// 		this.lastName = lastName;
+// 	}
+// 	name(){
+// 		return `${this.firstName} ${this.lastName}`;
+// 	}
+// 	whoAreYou(){
+// 		return `Hi I am ${this.name}`;
+// 	}
+// }
+// let person = new Person("Ibrahim", "Madi");
 //# sourceMappingURL=typescript-base.js.map
--- typescript-base.ts
+++ typescript-base.ts
@@ -55,3 +55,157 @@ const obj2 = {x:'ibr',...obj1}
 console.log(obj2);
 const { first:f,last:l} = obj1;
 console.log(f +' '+ l);
+// Array 
+const arr = ['a','b'];
+const[x,y]= arr;
+// console.log(arr);
+ const options = {x:'1',c:'2'};
+
+// Function Parameter Destructuring
+ function func(options){
+ 	console.log('Options X '+options.x);
+ }
+ func(options);
+function fun({x}) {
+console.log(x);
+}
+
+fun({x:"Forsan Test"});
+//For Loop
+for (var i =0; i< arr.length;i++) { //var ,let
+	
+	console.log(`For loop ES5 : ${arr[i]}`);
+}
+
+//For loop ES6 
+
+for(var i of arr){
+	console.log(`For Loop ES6 : ${i}`);
+}
+//Foreach
+arr.forEach(function(value){
+console.log(` This value with forEach ES5: ${value}`);
+});
+
+// For in Designed for interating over an object properites
+for(let i in obj1){
+	console.log(`For in  for used in object : ${i}`);
+}
+
+// Map & Set
+ let base = {a:1, b:2};
+ let objbase = Object.create(base);
+ // objbase[c] = 4;
+ for(i in objbase){
+ 	console.log(i)
+ }
+ console.log(`Object,create Error !!! ${objbase}`);
+
+let objoverriding = {hasOwnProperty:"test"};
+//  Error : TypeError: objoverriding.hasOwnProperty is not a function
+// objoverriding.hasOwnProperty("test Ovrriding");
+console.log(` Error !!! : ${objoverriding.hasOwnProperty}`);
+
+ let base1 = {a:1, __proto__:2};
+ for(i in base1){
+ 	console.log(`Property __proto__ !!!: ${i}`);
+ }
+
+ // Map 
+
+ let map = new Map([['A',3],['c',4]]);
+ console.log("Empty "+map.get("A"));
+
+ map.set('A',2);
+ // 
+ console.log("Set "+map.get("A"));// overriding
+ console.log("Set "+map.get("c"));
+ // for of looping
+ for(let entry of map.entries() ){
+ 	console.log(`For loop map  :  ${entry}`);
+ }
+
+
+ // for loop key & Value 
+ for(let [key,value] of map.entries()){
+ 	console.log(`Used Foreach to print key & Value with entries: ${key} -> ${value} `);
+ }
+
+
+ for(let [key,value] of map){
+ 	console.log(`Used Foreach to print key & Value : ${key} -> ${value} `);
+
+ }
+
+ for(let key of map.keys()){
+ 	console.log(`Map map.keys() : ${key}`);
+ }
+
+ for(let value of map.values()){
+ 	console.log(`Map map.values() : ${value}`);
+ }
+ // We can check to see if a key is present using the has method:
+ console.log("We can check to see if  a key is present using the has method : "+
+ 	map.has("A"));
+  console.log("We can check to see if  a key is present using the has method : "+
+ 	map.has("l"));
+  // We can check for the size (number of entries) using the size property:
+  console.log("We can check for thr size using the size method : "+
+  	map.size);
+  // We can delete entries using the delete method:
+   console.log("We can delete entries using the delete method: "+
+ 	map.delete("A"));
+// We can empty an entire Map by using the clear method:
+   console.log("We can empty an entries map by using the clear method: "+
+   	map.clear());
+
+// Set 
+console.log('Sets are a bit like maps but they only store keys not key-value pairs.');
+
+let set = new Set();
+set.add('Apple');
+console.log(set);
+
+// Api Promise
+let error = true;
+function doAsyncTask(){
+return new Promise((resolve, reject)=> {
+	setTimeout(()=>{
+		if (error) {
+			// code...
+			reject('error promise');
+		}else{
+			resolve('done promise');
+		}
+	},900);
+
+});
+}
+
+doAsyncTask().then(
+	(val)=> console.log(val),
+	(err)=>console.log(err)
+	);
+
+// Class
+
+
+// class Person{
+
+// 	firstName = "";
+// 	lastName = "";
+// 	constractor(firstName , lastName){
+// 		this.firstName = firstName;
+// 		this.lastName = lastName;
+// 	}
+
+// 	name(){
+// 		return `${this.firstName} ${this.lastName}`;
+// 	}
+
+// 	whoAreYou(){
+// 		return `Hi I am ${this.name}`;
+// 	}
+// }
+
+// let person = new Person("Ibrahim", "Madi");
